0b83eb5a26312a48af85c7c079ad2bedf511a865
jay-kr-g <gjaykumar0123@gmail.com>
2026-03-02 20:18:13 +0530

    Made more responsive

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const dbUrl = process.env.ATLASDB_URL;
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const dbUrl = process.env.ATLASDB_URL;
 
 main().then(()=>{
     console.log("connected to DB");
@@ -31,7 +31,7 @@ main().then(()=>{
 });
 
 async function main() {
-    await mongoose.connect(dbUrl);
+    await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine","ejs");
